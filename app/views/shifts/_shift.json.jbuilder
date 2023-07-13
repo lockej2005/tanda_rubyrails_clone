c629510fa586bfb65234b8f3cdad37e48ff0555c
lockej2005 <josh@intelligentinstallations.com.au>
2023-07-13 11:43:30 +1000

    adding models

---
+++ app/views/shifts/_shift.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! shift, :id, :start, :finish, :break_length, :user_id, :created_at, :updated_at
+json.url shift_url(shift, format: :json)
---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_014306) do
+  create_table "organisations", force: :cascade do |t|
+    t.string "name"
+    t.decimal "hourly_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.datetime "start"
+    t.datetime "finish"
+    t.integer "break_length"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shifts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "organisation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organisation_id"], name: "index_users_on_organisation_id"
+  end
+
+  add_foreign_key "shifts", "users"
+  add_foreign_key "users", "organisations"
+end
