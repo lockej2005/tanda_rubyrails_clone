3b29732bba3527ed2c630435968630dd2ec92c97
lockej2005 <josh@intelligentinstallations.com.au>
2023-07-13 14:56:45 +1000

    new changes update sdaf

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,4 @@
 class Shift < ApplicationRecord
   belongs_to :user
+  has_one :organisation, through: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :organisation
+  belongs_to :organisation, optional: true
+  has_many :shifts, dependent: :destroy
+  has_secure_password
 end
