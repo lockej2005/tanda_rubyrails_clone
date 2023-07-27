f5e117c9c447558d17bb7a5e9602256e44eb549a
lockej2005 <josh@intelligentinstallations.com.au>
2023-07-27 23:28:42 +1000

    small updates

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,17 @@
 class Shift < ApplicationRecord
   belongs_to :user
   belongs_to :organisation
+
+  def total_worked_hours
+    if start_time.present? && end_time.present?
+      total_hours = ((end_time - start_time) / 1.hour)
+      total_hours - (break_length / 60.0)
+    else
+      0
+    end
+  end
+
+  def total_pay
+    total_worked_hours * organisation.hourly_rate
+  end
 end
